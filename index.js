f4319a26ea30604dd1df7bb2bf239394d3ea635a
kimanibravel1806 <kimanibravel@gmail.com>
2026-03-24 17:41:29 +0300

    completed functions lab

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
 
+function isPalindrome(word) {
+    let reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
 
-
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let discount = (discountPercentage / 100) * originalPrice;
+    return originalPrice - discount;
+}
+module.exports = {
+    calculateTax,
+    convertToUpperCase,
+    findMaximum,
+    isPalindrome,
+    calculateDiscountedPrice
+};
